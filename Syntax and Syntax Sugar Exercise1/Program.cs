60f127f737e1c6e5c5e86738abb9013518419fbf
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-07 18:02:23 -0500

    initial commit

---
+++ Syntax and Syntax Sugar Exercise1/Program.cs
@@ -0,0 +1,8 @@
+﻿//inferred typing, string interpolation, ternary operator
+var answer = 4;
+string response = "";
+response = (answer < 9) ? $"{answer} is less than nine." :
+                                           $"{answer} is equal to or greater than nine.";
+Console.WriteLine(response);
+
+
